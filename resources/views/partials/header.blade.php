48371ab8a4b6dca81f29f62d1bf10ee14f61d078
Hernán Baravalle <hernan.baravalle@gmail.com>
2020-02-05 18:33:14 -0300

    Inicio del proyecto

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">WikiBanderas</a>
+            <div>
+                <a href="crear_pais.php">Crear país</a>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.mainLayout')
+@section('content')
+<section>
+    <div class="container">
+        <h1>Ad lorem ipsum</h1>
+    </div>
+</section>
+@endsection
